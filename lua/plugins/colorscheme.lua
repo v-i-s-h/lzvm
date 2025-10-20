72ff8d39616ad8c408de2cc019c9dcf8debbe623
Vishnu Raj <vishnu.raj@dolby.com>
2025-10-20 07:36:19 +0530

    colorscheme updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,14 @@
 local M = {
+  -- disable included themes
+  { "folke/tokyonight.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  -- enable vscode colorscheme
   {
     "Mofiqul/vscode.nvim",
     lazy = false,
     priority = 1000,
     name = "vscode",
     opts = function()
-      local c = require("vscode.colors").get_colors()
       return {
         -- Set style
         style = "dark",
@@ -16,22 +19,6 @@ local M = {
 
         -- Enable italic comment
         italic_comments = true,
-
-        -- Disable nvim-tree background color
-        -- disable_nvimtree_bg = true,
-
-        -- Override colors (see ./lua/vscode/colors.lua)
-        color_overrides = {
-          -- vscLineNumber = '#FFFFFF',
-          vscLineNumber = "#808080",
-        },
-
-        -- Override highlight groups (see ./lua/vscode/theme.lua)
-        group_overrides = {
-          -- this supports the same val table as vim.api.nvim_set_hl
-          -- use colors from this colorscheme by requiring vscode.colors!
-          Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
-        },
       }
     end,
   },
